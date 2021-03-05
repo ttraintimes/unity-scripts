26ad50dc5cf05983cdf5edfd50c17d5a374d7c2c
whereistt <mac@pc-129-31.customer.ask4.lan>
2021-03-05 02:08:01 +0000

    1

---
+++ zoomlook-wheel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateauto : MonoBehaviour {
+
+    private GameObject MyCube;
+    private float t;
+
+    void Start()
+    {
+        MyCube = GameObject.Find("Cube");
+        t = (float)1.5;
+    }
+
+    void Update()
+    {
+        MyCube.transform.Rotate(2*t, 2*t, 2*t);
+    }
+}
